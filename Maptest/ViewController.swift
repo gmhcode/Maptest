616bc12da10f8f0be044c0dd310da7a59840d1b8
Greg Hughes <greg.hughes@meritagepos.com>
2019-04-09 09:51:32 -0600

    Initial Commit

---
+++ Maptest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Maptest
+//
+//  Created by Greg Hughes on 4/9/19.
+//  Copyright © 2019 Greg Hughes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
